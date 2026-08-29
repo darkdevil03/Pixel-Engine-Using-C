410d0bb59594daab7cb22669fe310202ad93850c
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-08-29 21:37:59 +0530

    Pixel Engine project using Low-Level Lang-C

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+
+int main(void) {
+    SDL_Init(SDL_INIT_VIDEO );
+
+    SDL_Window  *window = SDL_CreateWindow("SDL Window", 320,200,SDL_WINDOWPOS_CENTERED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture *texture = SDL_CreateTexture(renderer,);
+
+    uint32_t framebuffer[320*200];
+
+    framebuffer[0] = 0x00FF00;
+
+    SDL_UpdateTexture(texture,NULL,framebuffer,sizeof(framebuffer));
+
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer,texture,framebuffer,sizeof(framebuffer));
+    SDL_RenderPresent(renderer);
+
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    return 0;
+}

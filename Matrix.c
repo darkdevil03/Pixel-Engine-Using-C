6f3a8a03882fd3f3f9f382019993c7e5a088af4d
darkdevil03 <bvvsatyanarayana280301@gmail.com>
2026-08-30 22:12:16 +0530

    Matrix implementation.

---
+++ Matrix.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int mat_arr[2][2]= {{2,2},{2,2}};
+
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            printf("%d ",mat_arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
